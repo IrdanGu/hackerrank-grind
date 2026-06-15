afb0ee233688a970c253e4e6f88cdbd8ded0fa0c
IrdanGu <20230910042@uniku.ac.id>
2026-06-15 14:19:54 +0700

    solve: page-count | math | easy

---
+++ python/2026-06-15_page-count.py
@@ -0,0 +1,47 @@
+# Problem   : Page Count
+# Topic     : math
+# Difficulty: easy
+# Date      : 2026-06-15
+# Source    : HackerRank
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER p
+#
+
+
+def pageCount(n, p):
+    # Turns from the front
+    front_turns = p // 2
+
+    # Turns from the back (Total turns in book - Turns to target page)
+    back_turns = (n // 2) - (p // 2)
+
+    # Return the minimum of the two
+    return min(front_turns, back_turns)
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
+
+    n = int(input().strip())
+
+    p = int(input().strip())
+
+    result = pageCount(n, p)
+
+    fptr.write(str(result) + "\n")
+
+    fptr.close()
